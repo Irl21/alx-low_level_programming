3f83719ac3e4725dee670161ca51925972e75b86
Irl21 <pwangai61@gmail.com>
2022-06-02 04:03:27 -0700

    test

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-
 /**
  * main - entry point
  * statements inside main
  * Return: always 0 
  */
-int main(void) 
+int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }

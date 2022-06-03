58a81d189d78842c176a07a852583f6d5d5502cd
Irl21 <pwangai61@gmail.com>
2022-06-03 07:15:12 -0700

    smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entrry point
+ * prints alphabets in reverse
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
+
+

b8dfa477b615aa5a2c6d5db6c8e06023101b1362
Irl21 <pwangai61@gmail.com>
2022-06-03 05:57:50 -0700

    prints alphabets lower and upper

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * print alphabets
+ * Return: always 0
+ */
+
+int main(void)
+{
+
+	char c;
+
+
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	for (c = 'A'; c <= 'Z'; c++)
+		putchar(c);
+
+	putchar('\n');
+
+
+
+	return (0);
+
+}

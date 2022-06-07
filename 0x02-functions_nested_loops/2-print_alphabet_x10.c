2f7b0baf55e8e3a8b4f2067de4276efa3efe4870
Irl21 <pwangai61@gmail.com>
2022-06-07 12:33:10 -0700

    prints 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include"main.h"
+
+/**
+ * print_alphabet_x10 - utilizes putchar prototype to print alphabets
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int c, n;
+
+	for (n = 0; n <= 10; n++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+			_putchar(c);
+		_putchar("\n");
+	}
+}

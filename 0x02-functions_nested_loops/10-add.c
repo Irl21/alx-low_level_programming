41881990ae1818d9c5225374719149f51d9bbf40
Irl21 <pwangai61@gmail.com>
2022-06-08 02:03:48 -0700

    sum of two inegers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include"main"
+/**
+ * add - adds two integers :i+j
+ * @i: first argument of the function
+ * @j; second argument of the function
+ * Return: results
+ */
+int add(int i, int j)
+{
+return (i + j);
+}

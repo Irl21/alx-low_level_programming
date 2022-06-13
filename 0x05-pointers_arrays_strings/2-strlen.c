90aa07815a80810e038620afcbfc5e1e9710310a
Irl21 <pwangai61@gmail.com>
2022-06-13 08:46:09 -0700

    calculates length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - calculates length of pointed string.
+ * @s: input
+ * Return: always 0
+ */
+
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s = 0)
+	{
+		length++;
+		s++;
+	}
+
+	return (length);
+}

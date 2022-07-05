98a155e3f27b4093a8167b379d88bae5934eaf12
Irl21 <pwangai61@gmail.com>
2022-07-05 07:04:13 -0700

    sums two numbers

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -2,10 +2,10 @@
 #define SUM_H
 
 /**
- * SUM: function like macro for sum
+ * 4-sum.h: function like macro for sum
  * @x: argument to macro
  * @y: second argument to macro
  */
 
-#define SUM(X, Y) ((X) + (Y))
+#define SUM(x, y) ((x) + (y))
 #endif

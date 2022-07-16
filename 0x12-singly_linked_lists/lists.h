055aeccecef212b33c15c723b8f91d2d73a59a46
Irl21 <pwangai61@gmail.com>
2022-07-16 00:42:30 -0700

    print lists

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,11 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-/**
-* Desc: header file containinf prototypes and definitions
-*	for all functions and types written in this proj
-*/
-
 #include <stdlib.h>
 
 /**
@@ -13,8 +8,9 @@
  * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
+ *
  * Description: singly linked list node structure
- *              for Holberton project
+ *              for alx.
  */
 
 typedef struct list_s

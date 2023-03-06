3a49b6c72de7f1a6f25068613b395c9c2e7bb946
root <pwangai61@gmail.com>
2023-03-06 12:03:38 +0000

    plague

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -4,13 +4,10 @@
  * hash_table_delete - Deletes a hash table.
  * @ht: A pointer to a hash table.
  */
-
 void hash_table_delete(hash_table_t *ht)
 {
 	hash_table_t *head = ht;
-
 	hash_node_t *node, *tmp;
-
 	unsigned long int i;
 
 	for (i = 0; i < ht->size; i++)
@@ -18,28 +15,16 @@ void hash_table_delete(hash_table_t *ht)
 		if (ht->array[i] != NULL)
 		{
 			node = ht->array[i];
-
 			while (node != NULL)
 			{
-
 				tmp = node->next;
-
 				free(node->key);
-
 				free(node->value);
-
 				free(node);
-
 				node = tmp;
-
 			}
-
 		}
-
 	}
-
 	free(head->array);
-
 	free(head);
-
 }
